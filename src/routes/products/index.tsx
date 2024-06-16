749752c708fcd2fe4c975afc84ad8369e57130f0
jean-philippe <jp@zelexio.com>
2024-06-16 14:19:26 -0400

    look ok

--- src/routes/products/index.tsx
+++ src/routes/products/index.tsx
@@ -1,18 +1,22 @@
 import { component$ } from "@builder.io/qwik";
 
+export const ProductCard = component$(() => {
+  return (
+    <div class="p-32  lg:p-36 h-1/2   border  rounded border-primary">
+      <h1 class="text-center">Card of Coffee</h1>
+
+    </div>
+  );
+});
+
+
 
 export const Tables = component$(() => {
   return (
-    <div class=" bg-white  justify-center flex text-black lg:flex-row flex-col lg:gap-5 lg:p-32 lg:py-12">
-      <div class="border rounded-xl border-main p-32 m-5  mb-1 lg:m-0 lg:p-48">
-        <h1>Card of Coffee</h1>
-      </div>
-      <div class="border rounded-xl border-main p-32 m-5  mb-1 lg:m-0 lg:p-48">
-        <h1>Card of Coffee</h1>
-      </div>
-      <div class="border rounded-xl border-main p-32 m-5 mb-1 lg:m-0 lg:p-48">
-        <h1>Card of Coffee</h1>
-      </div>
+    <div class=" bg-white  justify-center flex text-black  md:flex-col lg:flex-row flex-col  lg:gap-5  md:gap-2 py-12 px-12 gap-4 lg:px-32  lg:py-12">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
 
     </div >
   );
@@ -22,9 +26,12 @@ export const Tables = component$(() => {
 
 export default component$(() => {
   return (
-    <main class=" bg-gray-300 mt-5">
+    <main class=" mt-5 ">
       <Tables />
-      <h1>Home</h1>
+      <div class="py-40 h-[32.8em] content-center  bg-red-300">
+
+        <h1 class=" text-center ">Featured products ????</h1>
+      </div>
 
 
     </main>
