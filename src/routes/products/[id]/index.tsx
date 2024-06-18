8d17eaafe98a08d663a749e71bc508327b9d266b
jean-philippe martel <jpmartel98@gmail.com>
2024-06-17 21:10:13 -0400

    maybe

---
+++ src/routes/products/[id]/index.tsx
@@ -0,0 +1,10 @@
+import { component$ } from "@builder.io/qwik";
+import {} from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <main class="h-full    py-10  ">
+      <div class="text-red-800">hello</div>
+    </main>
+  );
+});
