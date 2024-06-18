cde9d3eccc21b9439c96830ad9b7085872db389c
jean-philippe <jp@zelexio.com>
2024-06-18 17:33:35 -0400

    nice stuff

--- src/routes/products/[name]/index.tsx
+++ src/routes/products/[name]/index.tsx
@@ -1,22 +1,28 @@
 import { component$ } from "@builder.io/qwik";
-import { useLocation } from "@builder.io/qwik-city";
+import { useLocation, } from "@builder.io/qwik-city";
+import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
+
   const location = useLocation();
   return (
-    <main class="grid   h-[80vh] grid-cols-2  bg-white  p-20">
-      <div class="justify-center rounded-xl bg-gray-100  text-center text-red-800">
+    <main class="grid   lg:h-[80.6vh] h-full lg:grid-cols-2  grid-flow-row bg-white  p-10 lg:p-20">
+      <div class="justify-center lg:h-full  h-[40vh] rounded-xl bg-gray-100  text-center text-red-800">
         <h1 class="  mt-4 text-4xl">{location.params.name}</h1>
       </div>
-      <div class=" mx-20 flex flex-col gap-2  self-center text-center align-bottom text-red-800">
-        <h1>
+      <div class=" lg:mx-20 flex flex-col gap-2 w-full self-center text-center align-bottom text-red-800 lg:h-fit lg:w-fit  ">
+        <h1 class=" flex w-full p-2 justify-end h-full self-center items-end ">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
         </h1>
         <button class="self-center rounded-lg bg-red-500 p-2  text-white">
           Click Me to know more
@@ -25,3 +31,14 @@ export default component$(() => {
     </main>
   );
 });
+
+export const head: DocumentHead = {
+  title: "best coffee you can buy",
+  meta: [
+    {
+      name: "description",
+      content: "smooth coffee is good",
+      media: "https://images.nightcafe.studio/jobs/Vkp6pDElnf3hXn1ncRRt/Vkp6pDElnf3hXn1ncRRt--1--387d3_5.9524x-real-esrgan-x4-plus.jpg?tr=w-1600,c-at_max",
+    },
+  ],
+};
