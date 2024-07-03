77b55ebf1ee19ec4b56349f17adaba0cfae3a1fb
jean-philippe <jp@zelexio.com>
2024-07-02 22:15:50 -0400

    look fine too me

--- src/routes/cart/index.tsx
+++ src/routes/cart/index.tsx
@@ -1,12 +1,33 @@
 import { component$ } from "@builder.io/qwik";
 export default component$(() => {
   return (
-    <main class=" my-4 flex  h-screen flex-row justify-center gap-20 bg-white p-8 pb-32 pt-16">
-      <div class=" h-full w-[40em] rounded border-2 border-secondary px-32 py-4">
-        <h1 class=" text-center text-2xl text-black">You Cart</h1>
+    <main class="  flex  h-full  flex-col md:flex-row justify-center gap-8 bg-white p-1  pb-32 py-16 mt-4 ">
+
+      <div class=" h-fit md:w-[40em] lg:w-[72em]  rounded border-2 border-secondary  mx-2  ">
+        <h1 class=" w-full  p-12 border-b-2 border-scondary_hover text-left text-2xl text-black">You Cart</h1>
+        <h1 class=" w-full  p-12 border-b-2 border-scondary_hover text-left text-2xl text-black">You Cart</h1>
+        <h1 class=" w-full  p-12 border-b-2 border-scondary_hover text-left text-2xl text-black">You Cart</h1>
       </div>
-      <div class=" mx-16  w-[20em] rounded  border-2 border-secondary py-4 text-2xl ">
-        <h1 class=" text-center text-2xl text-black"> buy a something </h1>
+      <div class="h-fit border-2  border-red-500  md:w-[28em] mx-3 mb-5 py-4">
+
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
+        <h1 class=" text-center text-2xl text-black px-8"> buy a something </h1>
       </div>
     </main>
   );
